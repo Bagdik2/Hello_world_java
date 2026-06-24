3c0474943d7368c4dd7b6b8fd4abc2973d6578ef
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-06-24 22:21:44 +0300

    patch_1.2.6

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -1,11 +1,39 @@
 package hi;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-
+		
+		//Инициализация переменных
+		byte varByte = 5;
+		short varShort = 4;
+		int varInt = 3;
+		long varLong = 999999L; 
+		float varFloat = 6.9f;
+		double varDouble = 3.333d;
+		boolean varBoolean = true;
+		char varChar = '\u006A';
+		
+		//Инициализация списка для переменных
+		List<Object> primitiveList = new ArrayList<>();
+		
+		//Добавление переменных в список
+		primitiveList.add(varByte   );
+		primitiveList.add(varShort  );
+		primitiveList.add(varInt    );
+		primitiveList.add(varLong   );
+		primitiveList.add(varFloat  );
+		primitiveList.add(varDouble );
+		primitiveList.add(varBoolean);
+		primitiveList.add(varChar   );
+		
+		//Вывод списка переменных для наглядности
+		for(Object element : primitiveList) {
+			System.out.println("Значение: " + element + " \t| Тип: " + element.getClass().getSimpleName());
+		}
 	}
 
 }
