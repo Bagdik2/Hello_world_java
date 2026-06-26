db114f06d475c265423c37e3e688abc5d8132361
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-06-26 17:16:37 +0300

    patch_1
    Commit for task 1.3.3

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -4,8 +4,27 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		System.out.printf("%c \n", charExpression(5));
+		System.out.printf("%d \n", getAgeDiff(Byte.parseByte("5"), (byte) 3));
 
 	}
+	
+	public static char charExpression(int a) {
+		char base = 'a';
+		return (char) (base + a);
+	}
 
+	public static byte getAgeDiff(byte age1, byte age2) {
+		if(age1 > age2) {
+			return (byte) (age1 % age2);
+		}
+		else if(age1 < age2) {
+			return (byte) (age2 % age1);
+		}
+		else if (age1 == age2) {
+			return (byte) (0);
+		}else {
+			return (byte) (0);
+		}
+	}
 }
