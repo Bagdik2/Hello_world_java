3e6b54ab4f92a9b9a2af30ca11798f8a863d3fc6
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-07-14 20:54:42 +0300

    patch_0
    Комит с инициализацией ветки задачи

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -3,9 +3,28 @@ package hi;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		//Выполнение первой задачи
+		String name = "Bogdan";
+		System.out.println(name);
+		//Выполнение второй задачи
+		parseAndPrintNumber("64");
+		parseAndPrintNumber("111");
+		//Выполнение третьей задачи
+		isPalindrome("Was it a cat I saw?");
 
 	}
-
+	public static void parseAndPrintNumber(String number) {
+		int returnNumber = Integer.parseInt(number) / 2;
+		System.out.println(returnNumber);
+	}
+	
+	public static void isPalindrome(String text) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(text);
+		sb.reverse();
+		String textBackflip = sb.toString();
+		System.out.println(textBackflip);
+		System.out.println(text.equalsIgnoreCase(textBackflip));
+	}
 }
+
