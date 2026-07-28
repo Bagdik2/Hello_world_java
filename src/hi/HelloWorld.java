60e4f4f327ceeee2c184314c953b2fd3a21489ae
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-07-28 12:06:01 +0300

    patch_0
    Инициализация методов задачи

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -1,6 +1,18 @@
 package hi;
 
 public class HelloWorld {
+	
+	public static boolean isWeekend(String dayName) {
+		return true;
+	}
+	
+	public static int weekendCount(String[] days) {
+		return 0;
+	}
+	
+	public static int weekdayCount(String[] days) {
+		return 0;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
