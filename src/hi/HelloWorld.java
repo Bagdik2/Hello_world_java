db152fc851eb565dd5b2447789a967dcb03f2d6d
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-07-28 20:09:46 +0300

    patch_02_final
    Оптимизированное и исправленное задание

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -14,47 +14,26 @@ public class HelloWorld {
 		
 	}
 	
-	//свой метод для DRY из задания
-	public static int myDay(String[] days, boolean bool) {
+	//2-й метод из задания
+	public static int weekendCount(String[] days) {
+		int weekendCounter = weekdayCount(days);
+		
+		return days.length - weekendCounter;
+	}
+	
+	//3-й метод из задания
+	public static int weekdayCount(String[] days) {
+
 		int dayCounter = 0;
 		
 		for (int i = 0; i < days.length; i++) {
-			if (isWeekend(days[i]) == bool) {
+			if (isWeekend(days[i]) == false) {
 				dayCounter++;
 			}
 		}
 		
 		return dayCounter;
-	}
-	
-	//2-й метод из задания
-	public static int weekendCount(String[] days) {
-		int weekendCounter = myDay(days, true);
-		
-		return weekendCounter;
-	}
-	
-	//3-й метод из задания
-	public static int weekdayCount(String[] days) {
-		int allowedDaysCount = 0;
-		for (int i = 0; i < days.length; i++) {
-			switch (days[i]) {
-			case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
-				allowedDaysCount++;
-				break;
-				
-			default:
-				break;
-			}
-		}
-//		String[] allowedDays = new String[allowedDaysCount];
-//		for (int i = 0; i < days.length; i++) {
-//			//if( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
-//		}
-		
-		int weekdayCounter = myDay(days, false);
-		
-		return weekdayCounter;
+
 	}
 
 	public static void main(String[] args) {
@@ -62,7 +41,6 @@ public class HelloWorld {
 		System.out.println("Hello world!");
 		String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 		String[] sevenFridays = new String[7];
-		String[] dirt = {"Картофель", "Tuesday", "Wednesday", "Яичница", "Friday", "Saturday", "Сапог"};
 		Arrays.fill(sevenFridays, "Friday"); 
 		System.out.println("Выходных в неделе:" + weekendCount(week));
 		System.out.println("Рабочих дней в неделе:" + weekdayCount(week));
