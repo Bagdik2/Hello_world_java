dd6989357cc3d6b1b43e9facba215fc31b67329b
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-08-03 08:37:19 +0300

    patch_0
    Инициализация классов, интерфейсов и методов .

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -7,5 +7,29 @@ public class HelloWorld {
 		System.out.println("Hello world!");
 
 	}
+	
+	interface TextAnalyzer {
+		Label processText(String text);
+	}
+	
+	enum Label {
+		SPAM, NEGATIVE_TEXT, TOO_LONG, OK
+	}
+	
+	class SpamAnalyzer implements TextAnalyzer {
+		
+	}
+	
+	class NegativeTextAnalyzer implements TextAnalyzer {
+		
+	}
+	
+	class TooLongTextAnalyzer implements TextAnalyzer {
+		
+	}
+	
+	Label[] analyzeText(TextAnalyzer[] analyzers, String text) {
+		
+	}
 
 }
