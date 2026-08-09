7f95324802ebaf93ae1ce3040308ca4c117b3b68
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-08-09 12:09:31 +0300

    patch_0
    Выполнил 1 подзадачу.
    Инициализировал нужные методы и классы.

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -5,7 +5,50 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		
+		//1 половина задания
+		
+		try(Car kar = new Car();) {
+			kar.drive();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		//2 половина задания
+		
+		
 
+	}
+	
+	public static class Car implements AutoCloseable {
+				
+		public void drive() {
+			System.out.println("Машина поехала.");
+		}
+		
+		@Override
+		public void close(){
+			System.out.println("Машина закрывается...");
+		}
+	}
+	
+	public static class BankClient extends Person {
+		
+		public BankClient(String firstName, String lastName) {
+			super();
+		}
+	}
+	
+//	public static interface BankWorker implements Person {
+//		
+//	}
+	
+	boolean checkClientForCredit(BankClient client) throws BadCreditHistoryException, ProblemWithLawException {
+		return false;
+	}
+	
+	void getCreditForClient(BankWorker worker, BankClient client) {
+		
 	}
 
 }
---
+++ src/hi/Person.java
@@ -0,0 +1,6 @@
+package hi;
+
+public interface Person {
+	public static final String firstName;
+	public static final String lastName;
+}
