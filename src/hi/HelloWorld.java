5c5b7ddb732eb9b7964ed9efbb7da9b749b0e5cb
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-09-09 15:05:19 +0300

    patch_02
    Исправленная задача

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -7,36 +7,31 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+
+		//Создаём ссылочный список LinkedList для нашего массива целых чисел
 		LinkedList<Integer> list = new LinkedList<>();
 		
+		//Создаём сканнер для ввода данных с клаваиатуры
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.println("Введите числа массива через пробел:");
 		
-//		String string = scanner.nextLine();
-		
-				
+		//Заполняем массив переменными из сканнера, целочисленными через пробел
 		while(scanner.hasNextInt()) {
 			list.add(scanner.nextInt());
 		}
 		
+		//Удаляем нечётные элементы и ноль
         for (int i = list.size() - 1; i >= 0; i--) {
             if (i % 2 != 0) { 
                 list.remove(i);
             }
         }
-//		Iterator<Integer> iterator = list.iterator();
-//        while (iterator.hasNext()) {
-//            int current = iterator.next();
-//            if (current % 2 != 0) { 
-//                iterator.remove();
-//            }
-//        }
-
+        list.remove(0);
+        
 		System.out.println("Ваши числа: " + list);
         Iterator<Integer> reverseIterator = list.descendingIterator();
+        
+        //Выводим перевёрнутый массив при помощи итератора
         while (reverseIterator.hasNext()) {
             System.out.print(reverseIterator.next() + " ");
         }
