3de2b339b8e94f29dad31f260e8e9e138065a72e
Bagdik_2 <59512906+Bagdik2@users.noreply.github.com>
2026-09-14 14:08:23 +0300

    patch_02
    Исправлен вывод

--- src/hi/HelloWorld.java
+++ src/hi/HelloWorld.java
@@ -2,26 +2,54 @@ package hi;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
-			System.out.println("Введите продажи работника в формате: \"Имя сумма\" (каждая запись с новой строки)");
-			System.out.println("Для завершения ввода нажмите Ctrl+D (Unix) или Ctrl+Z (Windows)");
+		//Старая версия ввода, нужно было вручную переводить пробел
+//		try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
+//			System.out.println("Введите продажи работника в формате: \"Имя сумма\" (каждая запись с новой строки)");
+//			System.out.println("Для завершения ввода нажмите Ctrl+D (Unix) или Ctrl+Z (Windows)");
+//			
+//			Map<String, Long> salesMap = getSalesMap(consoleReader);
+//			
+//            System.out.println("\nИтоговые продажи:");
+//            for (Map.Entry<String, Long> entry : salesMap.entrySet()) {
+//                System.out.println(entry.getKey() + " = " + entry.getValue());
+//            }
+//		} catch (IOException e) {
+//			System.err.println("Ошибка ввода-вывода: " + e.getMessage());
+//		}
+		//Создаём сканнер для считывания из консоли
+		Scanner scanner = new Scanner(System.in);
+		//Описываем работу программы в консоли для удобства
+		System.out.println("Введите данные как в примере:");
+		System.out.println("Алексей 3000 / Дмитрий 9000 / Антон 3000 / Алексей 7000");
+		//Записываем данные из сканнера в строковую переменную после первого перевода на новую строку и закрываем сканнер
+		String input = scanner.nextLine();
+		scanner.close();
+		//Добавляем регулярные выражения для раздела строки, как в примере через слеш, в том числе с пробелами, все слеши заменяем на 
+		String data = input.replaceAll("\\s*/\\s*", "\n");
+		
+		try (StringReader reader = new StringReader(data)) {
+			//Создаём мап с ключём/значением из ридера, созданного из строки что мы взяли из консоли выше
+			Map<String, Long> salesMap = getSalesMap(reader);
 			
-			Map<String, Long> salesMap = getSalesMap(consoleReader);
+			System.out.println("/nИтоговые продажи:");
+			//При помощи тернарного оператора построчно выводим сумму заработтанных продавцами денег
+			for (Map.Entry<String, Long> entry : salesMap.entrySet()) {
+				System.out.println(entry.getKey() + " = " + entry.getValue());
+			}
 			
-            System.out.println("\nИтоговые продажи:");
-            for (Map.Entry<String, Long> entry : salesMap.entrySet()) {
-                System.out.println(entry.getKey() + " = " + entry.getValue());
-            }
-		} catch (IOException e) {
-			System.err.println("Ошибка ввода-вывода: " + e.getMessage());
+		} catch (IOException e) { //просто выводим ошибку, если с вводом/выводом что-то не так
+			// TODO: handle exception
+			System.err.println("Ошибка обработки данных:" + e.getMessage());
 		}
 	}
 	
